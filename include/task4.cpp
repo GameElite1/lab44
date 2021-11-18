f3e358d222199b8278680a425017ef1fb6f4ccf0
oleg <olgecher35@gmail.com>
2021-11-18 20:54:24 +0300

    done all

--- include/task4.cpp
+++ include/task4.cpp
@@ -4,14 +4,14 @@
 //Copyright
 #include <header.hpp>
 void sort(std::vector<float>& a) {
-  int n;
+  int n, b;
   n = size(a);
   for (int i = 0; i < n; i++) {
     for (int j = n - 1; j > i; j--) {
       if (a[j - 1] > a[j]) {
-        a[n] = a[j - 1];
+        b = a[j - 1];
         a[j - 1] = a[j];
-        a[j] = a[n];
+        a[j] = b;
       }
     }
   }
